b03167093d24b7433c8d0b3c468de7e239134838
Vaibhav Garg <vaibigarg@gmail.com>
2012-03-16 06:33:31 +0000

    Updated for all changes as part of exercises

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,14 +1,22 @@
 class MoviesController < ApplicationController
 # This class is controller for Movies
   def index
-    @movies = Movie.all
+#@movies = Movie.find(:all, :order=>'release_date')
+   @movies = Movie.all
+   @movies = @movies.sort {|a,b| a.release_date <=> b.release_date}
   end
 
   def show 
-    id = params[:id]
-    @movie = Movie.find(id)
-#logger.debug("Value of Movie: " +  @movie.inspect) 
-    debugger
+      begin
+      id = params[:id]
+      @movie = Movie.find(id)
+      #logger.debug("Value of Movie: " +  @movie.inspect) 
+      debugger
+      a = @movie
+      rescue
+        flash[:warning] = "Specified Movie Not Found!"
+        redirect_to movies_path
+    end
   end
 
   def new
@@ -16,9 +24,27 @@ class MoviesController < ApplicationController
 
   def create 
     @movie = Movie.create!(params[:movie])
-    redirect_to movies_path
     flash[:notice] = "#{@movie.title} was successfully created"
+    redirect_to movie_path(@movie)
+    end
+ 
+  def edit
+    id = params[:id]
+    @movie = Movie.find(id)
   end
-  
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated"
+    redirect_to movies_path
+  end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} deleted"
+    redirect_to movies_path
+  end
 end
 
