3b8a32db5f70ba531dbe6602bfc35568f47d3377
Vaibhav Garg <vaibigarg@gmail.com>
2012-03-18 10:58:22 +0000

    uploaded

--- hw2_rottenpotatoes/app/controllers/movies_controller.rb
+++ hw2_rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,7 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    puts "Session is currently" + session.inspect
+    @movies = Movie.all(:order=> params[:sort])
+    @sortColumn = params[:sort]
+    @all_ratings = Array.new
+    @movies.each {|movie| @all_ratings << movie.rating}
+    @all_ratings = @all_ratings.uniq.sort 
+    if params.has_key?(:ratings)
+      @selectedRatings = params[:ratings]
+      @movies = @movies.delete_if {|movie| !@selectedRatings.has_key?(movie.rating)}
+    else
+      @selectedRatings = Hash.new 
+    end
+
+    @redirParams = Hash.new
+    if !params.has_key?(:sort) and session.has_key?(:sort)
+      @redirParams[:sort] = session[:sort]
+    elsif params.has_key?(:sort)
+      session[:sort] = params[:sort]
+    end
+
+    if !params.has_key?(:ratings) and session.has_key?(:ratings)
+      @redirParams[:ratings] = session[:ratings]
+    elsif params.has_key?(:ratings)
+      session[:ratings] = params[:ratings]
+    end
+    puts "Session is now" + session.inspect
+    puts "redir params is now: " + @redirParams.inspect
+    if @redirParams.length > 0 
+      redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+    end
+
   end
 
   def new
